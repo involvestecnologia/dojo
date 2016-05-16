5f883dbb5844a1acf39b225df9905f8b4708694e
fernandovsv <fernandovsv@gmail.com>
2016-05-16 12:09:37 -0300

    Second dojo

--- Bowling/src/Bowling.java
+++ Bowling/src/Bowling.java
@@ -1,26 +1,38 @@
 
 public class Bowling {
-	private int[] mScores = new int[21];
-	private int play;
-	boolean spare;
+	private int[] plays = new int[21];
+	private int currentPlay;
 
 	public void roll(int pins) {
-		if (pins < 0 || pins > 10){
+		if (pins < 0 || pins > 10) {
 			throw new IllegalArgumentException();
 		}
-		mScores[play++] = pins;
-		
-		if (play % 2 == 0) {
-			if (mScores[play-1] + mScores[play] == 10) {
-				spare = true;
-			}
-		}
-		
+		plays[currentPlay++] = pins;
+		if (pins == 10 && currentPlay % 2 != 0)
+			plays[currentPlay++] = 0;
+	}
+
+	private boolean isStrike(int i) {
+		return plays[i - 2] == 10;
+	}
+
+	private boolean isSpare(int i) {
+		return plays[i - 2] + plays[i - 1] == 10;
 	}
 
 	public int score() {
 		int score = 0;
-		
+
+		for (int i = 2; i < plays.length; i += 2) {
+			score += (plays[i - 2]) + (plays[i - 1]);
+
+			if (isStrike(i)) {
+				score += plays[i] + plays[i + 1];
+			} else if (isSpare(i)) {
+				score += plays[i];
+			}
+		}
+
 		return score;
 	}
 
--- Bowling/src/BowlingTest.java
+++ Bowling/src/BowlingTest.java
@@ -4,48 +4,77 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingTest {
-	
+
 	private Bowling bowling;
-	
+
 	@Before
 	public void bowling() {
 		bowling = new Bowling();
 	}
-	
-	@Test (expected = IllegalArgumentException.class) 
-	public void invalidMinInput(){
+
+	@Test(expected = IllegalArgumentException.class)
+	public void invalidMinInput() {
 		bowling.roll(-1);
 	}
-	
-	@Test (expected = IllegalArgumentException.class) 
-	public void invalidMaxInput(){
+
+	@Test(expected = IllegalArgumentException.class)
+	public void invalidMaxInput() {
 		bowling.roll(11);
 	}
-	
+
 	@Test
-	public void whenMissesAll_thenScoreIsZero(){
+	public void whenMissesAll_thenScoreIsZero() {
 		repeatRoll(0, 20);
 		assertEquals("Invalid score", 0, bowling.score());
 	}
-	
+
 	@Test
 	public void whenScoresOne20Times_thenScore20() {
 		repeatRoll(1, 20);
 		assertEquals("Invalid Score", 20, bowling.score());
 	}
-	
+
 	@Test
-	public void whenSpareOneTime_thenScoreIs() {
+	public void whenSpareOneTime_thenScoreIs12() {
 		bowling.roll(6);
 		bowling.roll(4);
 		bowling.roll(1);
 		repeatRoll(0, 17);
 		assertEquals(12, bowling.score());
 	}
-	
+
+	@Test
+	public void whenStrikeOneTime_thenScoreIs12() {
+		bowling.roll(10);
+		bowling.roll(1);
+		repeatRoll(0, 17);
+		assertEquals(12, bowling.score());
+	}
+
+	@Test
+	public void whenStrikeNotFirstTime_thenScoreIs14() {
+		bowling.roll(0);
+		bowling.roll(0);
+		bowling.roll(10);
+		bowling.roll(1);
+		bowling.roll(1);
+		repeatRoll(0, 15);
+		assertEquals(14, bowling.score());
+	}
+
+	@Test
+	public void whenTwoStrikeInARow_thenScoreIs35() {
+		bowling.roll(10);
+		bowling.roll(10);
+		bowling.roll(1);
+		bowling.roll(1);
+		repeatRoll(0, 14);
+		assertEquals(35, bowling.score());
+	}
+
 	private void repeatRoll(int score, int times) {
-		for (int count = 0; count < times; count++){
-			bowling.roll(score);	
+		for (int count = 0; count < times; count++) {
+			bowling.roll(score);
 		}
 	}
 
