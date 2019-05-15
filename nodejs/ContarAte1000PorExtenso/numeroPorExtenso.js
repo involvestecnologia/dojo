16349836c73ffed892a41de51aa6a724a225722e
RaphaSS <raphael.silveira@involves.com.br>
2019-05-15 14:36:04 -0300

    Continuação do desafio de números por extenso

--- nodejs/ContarAte1000PorExtenso/numeroPorExtenso.js
+++ nodejs/ContarAte1000PorExtenso/numeroPorExtenso.js
@@ -1,129 +1,188 @@
-const unidadePorExtenso = function(numero) {
-    switch(numero){
+const unidadePorExtenso = function (numero) {
+    switch (numero) {
         case 0:
-        return "zero";
+            return "zero";
 
         case 1:
-        return "um";
+            return "um";
 
         case 2:
-        return 'dois';
+            return 'dois';
 
         case 3:
-        return 'tres';
+            return 'tres';
 
         case 4:
-        return 'quatro';
+            return 'quatro';
 
         case 5:
-        return 'cinco';
+            return 'cinco';
 
         case 6:
-        return 'seis';
+            return 'seis';
 
         case 7:
-        return 'sete';
+            return 'sete';
 
         case 8:
-        return 'oito';
+            return 'oito';
 
         case 9:
-        return 'nove';
+            return 'nove';
 
         default:
-        return '';
+            return '';
     }
-    
+
 }
 
 const dezenaPorExtenso = (numero) => {
     switch (numero) {
         case 10:
-        return 'dez';
+            return 'dez';
 
         case 11:
-        return 'onze';
+            return 'onze';
 
         case 12:
-        return 'doze';
+            return 'doze';
 
         case 13:
-        return 'treze';
+            return 'treze';
 
         case 14:
-        return 'quatorze';
+            return 'quatorze';
 
         case 15:
-        return 'quinze';
+            return 'quinze';
 
         case 16:
-        return 'dezesseis';
+            return 'dezesseis';
 
         case 17:
-        return 'dezessete';
+            return 'dezessete';
 
         case 18:
-        return 'dezoito';
+            return 'dezoito';
 
         case 19:
-        return 'dezenove';
+            return 'dezenove';
 
         default:
     }
 }
 
 const dezenasRegularesPorExtenso = (numero) => {
-    switch (numero){
+    switch (numero) {
         case 2:
-        return 'vinte';
+            return 'vinte';
 
         case 3:
-        return 'trinta';
+            return 'trinta';
 
         case 4:
-        return 'quarenta';
+            return 'quarenta';
 
         case 5:
-        return 'cinquenta';
+            return 'cinquenta';
 
         case 6:
-        return 'sessenta';
+            return 'sessenta';
 
         case 7:
-        return 'setenta';
+            return 'setenta';
 
         case 8:
-        return 'oitenta';
+            return 'oitenta';
 
         case 9:
-        return 'noventa';
+            return 'noventa';
     }
 }
 
+const centenasPorExtenso = (numero) => {
+    switch (numero) {
+        case 1:
+            return 'cento';
+
+        case 2:
+            return 'duzentos';
+
+        case 3:
+            return 'trezentos';
+
+        case 4:
+            return 'quatrocentos';
+
+        case 5:
+            return 'quinhentos';
+
+        case 6:
+            return 'seiscentos';
+
+        case 7:
+            return 'setecentos';
+
+        case 8:
+            return 'oitocentos';
+
+        case 9:
+            return 'novecentos';
+    }
+};
+
 const numeroPorExtenso = (numero) => {
 
-    if(numero < 10){
-        return unidadePorExtenso(numero);
+    if (numero < 10) {
+        return trataUnidade(numero);
     }
 
-    if(numero >= 10 && numero < 20){
+    if (numero >= 10 && numero < 20) {
         return dezenaPorExtenso(numero);
-    }        
-        
-    if(numero >= 20 && numero < 100){
+    }
+
+    if (numero >= 20 && numero < 100) {
         let numeroStr = numero.toString();
-        let numeroArr = numeroStr.split('').map(n =>  parseInt(n));
+        let numeroArr = numeroStr.split('').map(n => parseInt(n));
         let dezena = numeroArr[0];
         let unidade = numeroArr[1];
 
-        if(unidade === 0){
+        if (unidade === 0) {
             return dezenasRegularesPorExtenso(dezena);
         }
-            
+
         return dezenasRegularesPorExtenso(dezena) + 'e' + unidadePorExtenso(unidade);
     }
 
+    if (numero >= 100) {
+        let numeroStr = numero.toString();
+        let numeroArr = numeroStr.split('').map(n => parseInt(n));
+        let centena = numeroArr[0];
+        let dezena = numeroArr[1];
+        let unidade = numeroArr[2];
+
+        if (centena == 1 && unidade == 0 && dezena == 0) {
+            return 'cem';
+        }
+
+        let resultado = centenasPorExtenso(centena);
+
+        if (dezena != 0) {
+            resultado += 'e' + dezenaPorExtenso(dezena);
+        }
+
+        if (unidade != 0) {
+            resultado += 'e' + unidadePorExtenso(unidade);
+        }
+
+        return resultado;
+    }
+
     throw new Error("Numero valido não tratado");
+}
+
+const trataUnidade = unidade => {
+    return unidadePorExtenso(numero);
 }
 
 module.exports.numeroPorExtenso = numeroPorExtenso;
--- nodejs/ContarAte1000PorExtenso/numeroPorExtenso.spec.js
+++ nodejs/ContarAte1000PorExtenso/numeroPorExtenso.spec.js
@@ -1,140 +1,196 @@
-const {assert} = require('chai');
-const {numeroPorExtenso} = require('./numeroPorExtenso');
+const { assert } = require('chai');
+const { numeroPorExtenso } = require('./numeroPorExtenso');
 
 suite('Coding dojo', () => {
 
-    describe('Teste de unidades', ()=>{
+    describe('Teste de unidades', () => {
 
-        it('deve identificar se a casa de unidade eh zero', ()=>{
+        it('deve identificar se a casa de unidade eh zero', () => {
             const zero = 0;
             assert.equal(numeroPorExtenso(zero), "zero");
         });
 
-        it('deve identificar se a casa de unidade eh um', ()=>{
+        it('deve identificar se a casa de unidade eh um', () => {
             const um = 1;
             assert.equal(numeroPorExtenso(um), "um");
         });
 
-        it('deve identificar se a casa de unidade eh dois', ()=>{
+        it('deve identificar se a casa de unidade eh dois', () => {
             const dois = 2;
             assert.equal(numeroPorExtenso(dois), "dois");
         });
 
-        it('deve identificar se a casa de unidade eh tres', ()=>{
+        it('deve identificar se a casa de unidade eh tres', () => {
             const tres = 3;
             assert.equal(numeroPorExtenso(tres), "tres");
         });
 
-        it('deve identificar se a casa de unidade eh quatro', ()=>{
+        it('deve identificar se a casa de unidade eh quatro', () => {
             const quatro = 4;
             assert.equal(numeroPorExtenso(quatro), "quatro");
         });
 
-        it('deve identificar se a casa de unidade eh cinco', ()=>{
+        it('deve identificar se a casa de unidade eh cinco', () => {
             const cinco = 5;
             assert.equal(numeroPorExtenso(cinco), "cinco");
         });
 
-        it('deve identificar se a casa de unidade eh seis', ()=>{
+        it('deve identificar se a casa de unidade eh seis', () => {
             const seis = 6;
             assert.equal(numeroPorExtenso(seis), "seis");
         });
 
-        it('deve identificar se a casa de unidade eh sete', ()=>{
+        it('deve identificar se a casa de unidade eh sete', () => {
             const sete = 7;
             assert.equal(numeroPorExtenso(sete), "sete");
         });
 
-        it('deve identificar se a casa de unidade eh oito', ()=>{
+        it('deve identificar se a casa de unidade eh oito', () => {
             const oito = 8;
             assert.equal(numeroPorExtenso(oito), "oito");
         });
 
-        it('deve identificar se a casa de unidade eh nove', ()=>{
+        it('deve identificar se a casa de unidade eh nove', () => {
             const nove = 9;
             assert.equal(numeroPorExtenso(nove), "nove");
-        });        
+        });
 
     });
 
-    describe('Teste de dezenas', () =>  {
-        it('deve identificar se a casa de dezena eh dez', ()=>{
+    describe('Teste de dezenas', () => {
+        it('deve identificar se a casa de dezena eh dez', () => {
             assert.equal(numeroPorExtenso(10), "dez");
-        }); 
+        });
 
-        it('deve identificar se a casa de dezena eh onze', ()=>{
+        it('deve identificar se a casa de dezena eh onze', () => {
             assert.equal(numeroPorExtenso(11), "onze");
-        }); 
+        });
 
-        it('deve identificar se a casa de dezena eh doze', ()=>{
+        it('deve identificar se a casa de dezena eh doze', () => {
             assert.equal(numeroPorExtenso(12), "doze");
-        }); 
+        });
 
-        it('deve identificar se a casa de dezena eh treze', ()=>{
+        it('deve identificar se a casa de dezena eh treze', () => {
             assert.equal(numeroPorExtenso(13), "treze");
-        }); 
+        });
 
-        it('deve identificar se a casa de dezena eh quatorze', ()=>{
+        it('deve identificar se a casa de dezena eh quatorze', () => {
             assert.equal(numeroPorExtenso(14), "quatorze");
-        }); 
+        });
 
-        it('deve identificar se a casa de dezena eh quinze', ()=>{
+        it('deve identificar se a casa de dezena eh quinze', () => {
             assert.equal(numeroPorExtenso(15), "quinze");
         });
 
-        it('deve identificar se a casa de dezena eh dezesseis', ()=>{
+        it('deve identificar se a casa de dezena eh dezesseis', () => {
             assert.equal(numeroPorExtenso(16), "dezesseis");
         });
 
-        it('deve identificar se a casa de dezena eh dezessete', ()=>{
+        it('deve identificar se a casa de dezena eh dezessete', () => {
             assert.equal(numeroPorExtenso(17), "dezessete");
         });
 
-        it('deve identificar se a casa de dezena eh dezoito', ()=>{
+        it('deve identificar se a casa de dezena eh dezoito', () => {
             assert.equal(numeroPorExtenso(18), "dezoito");
         });
 
-        it('deve identificar se a casa de dezena eh dezenove', ()=>{
+        it('deve identificar se a casa de dezena eh dezenove', () => {
             assert.equal(numeroPorExtenso(19), "dezenove");
         });
 
-        it('deve identificar se a casa de dezena eh vinte', ()=>{
+        it('deve identificar se a casa de dezena eh vinte', () => {
             assert.equal(numeroPorExtenso(20), "vinte");
         });
 
-        it('deve identificar se a casa de dezena eh trinta', ()=>{
+        it('deve identificar se a casa de dezena eh trinta', () => {
             assert.equal(numeroPorExtenso(30), "trinta");
         });
 
-        it('deve identificar se a casa de dezena eh quarenta', ()=>{
+        it('deve identificar se a casa de dezena eh quarenta', () => {
             assert.equal(numeroPorExtenso(40), "quarenta");
         });
 
-        it('deve identificar se a casa de dezena eh cinquenta', ()=>{
+        it('deve identificar se a casa de dezena eh cinquenta', () => {
             assert.equal(numeroPorExtenso(50), "cinquenta");
         });
 
-        it('deve identificar se a casa de dezena eh sessenta', ()=>{
+        it('deve identificar se a casa de dezena eh sessenta', () => {
             assert.equal(numeroPorExtenso(60), "sessenta");
         });
 
-        it('deve identificar se a casa de dezena eh setenta', ()=>{
+        it('deve identificar se a casa de dezena eh setenta', () => {
             assert.equal(numeroPorExtenso(70), "setenta");
         });
 
-        it('deve identificar se a casa de dezena eh oitenta', ()=>{
+        it('deve identificar se a casa de dezena eh oitenta', () => {
             assert.equal(numeroPorExtenso(80), "oitenta");
         });
 
-        it('deve identificar se a casa de dezena eh noventa', ()=>{
+        it('deve identificar se a casa de dezena eh noventa', () => {
             assert.equal(numeroPorExtenso(90), "noventa");
         });
     });
 
     describe('Testes de combinação de dezenas com unidades', () => {
 
-        it('deve concatenar dezena e unidade corretamente', () => {
+        it('deve concatenar dezena e unidade corretamente (21)', () => {
             assert.equal(numeroPorExtenso(21), 'vinteeum');
+        });
+
+        it('deve concatenar dezena e unidade corretamente (99)', () => {
+            assert.equal(numeroPorExtenso(99), 'noventaenove');
+        });
+
+    });
+
+    describe('Testes de centenas', () => {
+
+        it('deve identificar se a casa de centena eh cem', () => {
+            assert.equal(numeroPorExtenso(100), "cem");
+        });
+
+        it('deve identificar se a casa de centena eh duzentos', () => {
+            assert.equal(numeroPorExtenso(200), "duzentos");
+        });
+
+        it('deve identificar se a casa de centena eh trezentos', () => {
+            assert.equal(numeroPorExtenso(300), "trezentos");
+        });
+
+        it('deve identificar se a casa de centena eh quatrocentos', () => {
+            assert.equal(numeroPorExtenso(400), "quatrocentos");
+        });
+
+        it('deve identificar se a casa de centena eh quinhentos', () => {
+            assert.equal(numeroPorExtenso(500), "quinhentos");
+        });
+
+        it('deve identificar se a casa de centena eh seiscentos', () => {
+            assert.equal(numeroPorExtenso(600), "seiscentos");
+        });
+
+        it('deve identificar se a casa de centena eh setecentos', () => {
+            assert.equal(numeroPorExtenso(700), "setecentos");
+        });
+
+        it('deve identificar se a casa de centena eh oitocentos', () => {
+            assert.equal(numeroPorExtenso(800), "oitocentos");
+        });
+
+        it('deve identificar se a casa de centena eh novecentos', () => {
+            assert.equal(numeroPorExtenso(900), "novecentos");
+        });
+
+    });
+
+    describe('Teste de combinação com centenas, dezenas e unidades', () => {
+
+        it('deve concatenar centena e unidade corretamente (101)', () => {
+            assert.equal(numeroPorExtenso(101), 'centoeum');
+        });
+
+        it('deve concatenar centena e dezena corretamente (110)', () => {
+            assert.equal(numeroPorExtenso(110), 'centoedez');
         });
 
     });
