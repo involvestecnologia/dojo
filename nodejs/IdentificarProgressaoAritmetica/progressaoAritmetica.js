4345f31c136fb7100fb26caa805f1bb49e09032a
Kadio Colzani <kadio.colzani@INV000682.involves.corp>
2019-05-29 19:41:07 -0300

    inicio do desafio de identificacoes de progressoes aritmeticas

---
+++ nodejs/IdentificarProgressaoAritmetica/progressaoAritmetica.js
@@ -0,0 +1,43 @@
+const _ = require('lodash');
+
+const identificarSubconjuntos = conjunto => {
+  checkIfIsArray(conjunto);
+  checkIfLengthIsGreaterThan3(conjunto);
+  checkSetContainsOnlyValidNumbers(conjunto);
+  conjunto = sort(conjunto);
+
+
+};
+
+const identificaRazoes = conjunto => {
+
+};
+
+const sort = conjunto => _.sortBy(conjunto);
+
+const checkIfIsArray = conjunto => {
+  if(!Array.isArray(conjunto)) {
+    throw new Error('Conjunto deve ser um array');
+  }
+};
+const checkIfLengthIsGreaterThan3 = conjunto => {
+  if(conjunto.length < 3){
+    throw new Error('Conjunto deve conter no mínimo 3 números inteiros.')
+  }
+};
+const checkSetContainsOnlyValidNumbers = conjunto => {
+  conjunto.forEach(numero => {
+    if(isNaN(numero)){
+      throw new Error('Conjunto deve conter apenas números')
+    }
+
+    if(numero < 0 ) {
+      throw new Error('Conjunto deve conter apenas números positivos');
+    }
+  });
+};
+
+module.exports = {
+  identificarSubconjuntos,
+  identificaRazoes
+};
---
+++ nodejs/IdentificarProgressaoAritmetica/progressaoAritmetica.spec.js
@@ -0,0 +1,30 @@
+const { assert, expect } = require('chai');
+const progressaoAritmetica = require('./progressaoAritmetica');
+
+suite('Identificando Progressão Aritmética em um conjunto de números', () => {
+  describe('Tratamento do conjunto de entrada', () => {
+    it('Conjunto deve ser um array', () => {
+      assert.throws(() => progressaoAritmetica.identificarSubconjuntos('teste'), Error);
+    });
+
+    it('Conjunto deve possuir 3 ou mais números', () => {
+      assert.throws(() => progressaoAritmetica.identificarSubconjuntos([1,2]), Error);
+    });
+
+    it('Conjunto deve ter apenas numeros', () =>{
+      assert.throws(() => progressaoAritmetica.identificarSubconjuntos([1,3,5,"A"]), Error);
+    });
+
+    it('Conjunto deve ter apenas numeros positivos', () =>{
+      assert.throws(() => progressaoAritmetica.identificarSubconjuntos([1,3,5,-2]), Error);
+    });
+
+  });
+
+  describe('Identificacao de razoes', () => {
+    it('Retorna todas as razoes do conjunto [1,2,3]', () => {
+      expect(progressaoAritmetica.identificaRazoes([1,2,3])).to.eql([1,2]);
+    });
+
+  })
+});
