5b25bfc2d5a11feba0584b9746362ac55f2e0485
Jorge Maronezzi <jorge.maronezzi@involves.com.br>
2019-12-04 18:56:51 -0300

    + Criado base para dojo de EntradaNoBanco

---
+++ nodejs/EntradasNoBanco/entrada-no-banco.spec.js
@@ -0,0 +1,10 @@
+const { assert } = require('chai');
+const entrada = require('./entrada-no-banco');
+
+suite('Valida entradas de hora no banco', () => {
+  describe('Valida se entrada esta em horário comercial', () => {
+    it('para uma entrada', () => {
+      assert.equal(este, aquele);
+    });
+  });
+});
